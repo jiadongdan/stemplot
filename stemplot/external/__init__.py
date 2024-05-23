e71054c8b110e3ecfa63e3c8e593b98b455fc5df
jiadongdan <ted930511@gmail.com>
2024-05-23 20:08:31 +0800

    Create __init__.py

---
+++ stemplot/external/__init__.py
@@ -0,0 +1,7 @@
+# Import the function chord_diagram from the external module and rename it to plot_chord_diagram
+from mpl_chord_diagram import chord_diagram as plot_chord_diagram
+
+__all__ = ['plot_chord_diagram',
+           ]
+
+
