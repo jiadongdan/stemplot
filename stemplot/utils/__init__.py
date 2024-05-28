8056f40d45139470e3030cc44434ae1c77e3ad0c
jiadongdan <ted930511@gmail.com>
2024-05-28 12:21:19 +0800

    update notebook

--- stemplot/utils/__init__.py
+++ stemplot/utils/__init__.py
@@ -2,9 +2,11 @@ from ._plot_density import plot_density
 from ._plot_pca import plot_pca
 from ._plot_image import plot_image
 from ._heatmap import plot_heatmap
+from ._heatmap import annotate_heatmap
 
 __all__ = ['plot_density',
            'plot_pca',
            'plot_image',
            'plot_heatmap',
+           'annotate_heatmap',
            ]
